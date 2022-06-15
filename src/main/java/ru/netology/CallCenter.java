5221ec7600c2aeef73028834177dd97fce2d6488
AndreyS <andro76as@mail.com>
2022-06-15 12:29:51 +0300

    Commit

--- src/main/java/ru/netology/CallCenter.java
+++ src/main/java/ru/netology/CallCenter.java
@@ -3,17 +3,25 @@ package ru.netology;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
-import static ru.netology.Main.calls;
 
 public class CallCenter {
+
+    public static final int calls = 15;
+    public static final int specialist = 4;
+
     // 1 блокировка потоков
     // 2 честные блокировки (следит зо очередью)
     private static final Queue<String> phoneCalls = new ArrayBlockingQueue<>(calls); // размер буфера
 
     public void call() {
-        phoneCalls.offer(Thread.currentThread().getName());
+        //phoneCalls.offer(Thread.currentThread().getName());
+        for (int i = 1; i <= calls; i++) {
+            String call = "запрос " + i;
+            phoneCalls.add(call);
+            System.out.printf("%s звонок добавлен в очередь %s\n", Thread.currentThread().getName(), call);
+        }
         try {
-            Thread.sleep(3000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -21,13 +29,20 @@ public class CallCenter {
     }
 
     public void takeTheCall() {
-        while (!phoneCalls.isEmpty()) {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        try {
+            Thread.sleep(2000);
+            while (true) {
+
+                if (phoneCalls.isEmpty()) {
+                    break;
+                } else {
+                    System.out.println("Специалист " + Thread.currentThread().getName() + " ответил на звонок " + phoneCalls.poll());
+                }
             }
-            System.out.println("Специалист " + Thread.currentThread().getName() + " ответил на звонок " + phoneCalls.poll());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+
 }
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,17 +1,21 @@
 package ru.netology;
 
+import static ru.netology.CallCenter.specialist;
+
 public class Main {
-    public static final int calls = 60;
-    public static final int specialist = 4;
+
 
     public static void main(String[] args) {
         CallCenter center = new CallCenter();
-        for (int i = 1; i <= calls; i++) {
-            new Thread(null, center::call, "№ " + i).start();
-        }
+        Thread atc = new Thread(center::call, "Колл-центр ");
+
+//        for (int i = 1; i <= calls; i++) {
+//            new Thread(null, center::call, "№ " + i).start();
+//        }
 
         for (int i = 1; i <= specialist; i++) {
             new Thread(null, center::takeTheCall, "№ " + i).start();
         }
+        atc.start();
     }
 }
